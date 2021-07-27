b18ad3885d2225881f1dfc119c1e755c104012c5
matsuchiyo <macho.macho.dev@gmail.com>
2021-07-27 15:15:57 +0900

    Overlapping RecyclerView sample: OverlappingItemDecoration

--- OverlappingRecyclerViewSample/app/src/main/java/com/example/overlappingrecyclerviewsample/MainViewModel.kt
+++ OverlappingRecyclerViewSample/app/src/main/java/com/example/overlappingrecyclerviewsample/MainViewModel.kt
@@ -1,2 +1,44 @@
 package com.example.overlappingrecyclerviewsample
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+data class Item(val name: String)
+
+class MainViewModel: ViewModel() {
+    val items: LiveData<List<Item>> = MutableLiveData<List<Item>>(
+        listOf(
+            Item("aaa"),
+            Item("bbb"),
+            Item("ccc"),
+            Item("ddd"),
+            Item("eee"),
+            Item("aaa"),
+            Item("bbb"),
+            Item("ccc"),
+            Item("ddd"),
+            Item("eee"),
+            Item("aaa"),
+            Item("bbb"),
+            Item("ccc"),
+            Item("ddd"),
+            Item("eee"),
+            Item("aaa"),
+            Item("bbb"),
+            Item("ccc"),
+            Item("ddd"),
+            Item("eee"),
+            Item("aaa"),
+            Item("bbb"),
+            Item("ccc"),
+            Item("ddd"),
+            Item("eee"),
+            Item("aaa"),
+            Item("bbb"),
+            Item("ccc"),
+            Item("ddd"),
+            Item("eee"),
+        )
+    )
+}
